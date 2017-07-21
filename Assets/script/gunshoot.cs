b9821419f49da604334bfbfedcdd55bfe815e645
Jacob Gelder <jacob@bocracy.com>
2017-07-21 14:56:50 -0600

    Added gun and water

---
+++ Assets/script/gunshoot.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gunshoot : MonoBehaviour {
+	private Transform playerTransform;
+	private RaycastHit hit;
+	public float range = 400;
+
+	// Use this for initialization
+	void Start () {
+		playerTransform = transform.parent;
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire1")) {
+			Debug.Log ("Shot");
+
+			if (Physics.Raycast (playerTransform.TransformPoint (new Vector2 (Random.Range (-1, 1), Random.Range(-1, 1))), playerTransform.forward, out hit, range)) {
+				if (hit.collider.tag == "Enemy") {
+					Destroy (hit.transform.gameObject);
+					Debug.Log ("shootEnemy");
+				}
+			}
+		}
+	}
+}
